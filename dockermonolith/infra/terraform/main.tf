1ed2a113c50980aa598b862e20edca53a980215f
dmnbars <dmnbars@gmail.com>
2021-03-23 10:36:46 +0300

    добавлен прототип terraform конфига для запуска инстансов

---
+++ dockermonolith/infra/terraform/main.tf
@@ -0,0 +1,35 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_compute_instance" "app" {
+  count = var.instance_count
+  name  = "reddit-monolith-instance-${count.index}"
+
+  labels = {
+    tags = "reddit-monolith-instance"
+  }
+
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
